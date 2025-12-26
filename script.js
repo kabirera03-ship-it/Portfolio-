42f8463800b634fbe6895a86d9cabf88a94bec83
kabirera03-ship-it <kabirera03@gmail.com>
2025-12-26 17:46:42 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Portfolio Loaded");
+// Simple fade-in animation
+document.querySelectorAll("section").forEach(section => {
+  section.style.opacity = 0;
+  section.style.transform = "translateY(30px)";
+
+  setTimeout(() => {
+    section.style.transition = "0.8s";
+    section.style.opacity = 1;
+    section.style.transform = "translateY(0)";
+  }, 200);
+}); Loaded");
